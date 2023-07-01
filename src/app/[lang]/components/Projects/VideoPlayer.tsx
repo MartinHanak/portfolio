5cc959e87dbbdef497c00450e9955e50e8c9d2a2
Martin Hanák <martinhanak97@gmail.com>
2023-07-01 19:48:34 +0200

    Add projects videos

--- src/app/[lang]/components/Projects/VideoPlayer.tsx
+++ src/app/[lang]/components/Projects/VideoPlayer.tsx
@@ -1,7 +1,7 @@
 "use client"
 // DOES NOT WORK WITHOUT CHECKING WINDOW:   import ReactPlayer from "react-player"
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 export interface VideoPlayer {
@@ -9,12 +9,16 @@ export interface VideoPlayer {
 }
 
 export function VideoPlayer({ videoURL }: VideoPlayer) {
+
+    const [playing, setPlaying] = useState(false);
+
     return (
         <Suspense fallback={<div className="w-full h-full bg-white"></div>}>
-            <div className="w-full aspect-video bg-white">
+            <div className="w-full aspect-video bg-white"
+                onClick={() => setPlaying(true)}>
 
                 <ReactPlayer
-                    url={videoURL} width='100%' height='100%' muted playing loop
+                    url={videoURL} width='100%' height='100%' muted light playing={playing} loop
                     config={{
                         file: {
                             attributes: {
